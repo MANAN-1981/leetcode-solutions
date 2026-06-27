56d0bc6ddc603d197e5250aad19e16e26b6c443e
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-06-28 00:17:02 +0530

    Add merge-sorted-array [Array] [Easy] (Java)

---
+++ Array/Easy/merge-sorted-array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       
+        for(int i=m; i<nums1.length; i++){
+            if(nums1[i] == 0){
+                nums1[i] = nums2[n-1];
+                n--;
+            }
+        }
+        Arrays.sort(nums1);
+    }
+}
