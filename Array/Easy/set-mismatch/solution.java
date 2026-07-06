8492c7aeb8eb1b33de6901dc6550e958ed4666ef
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-07-06 22:51:01 +0530

    Add set-mismatch [Array] [Easy] (Java)

---
+++ Array/Easy/set-mismatch/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        
+        	int n = nums.length;
+	    Arrays.sort(nums);
+	    
+	    int duplicate =-1;
+	    int missing = -1;
+	    
+	    for(int i=1; i<n; i++){
+	        if(nums[i] == nums[i-1]){
+	            duplicate = nums[i];
+	        }else if(nums[i] - nums[i-1] == 2){
+	            missing = nums[i]-1;
+	        }
+	    }
+	    if(nums[0] != 1){
+	        missing = 1;
+	    }
+	    if(nums[n-1] != n){
+                missing = n;
+	    }
+          
+		return new int[] {duplicate , missing};
+    }
+}
