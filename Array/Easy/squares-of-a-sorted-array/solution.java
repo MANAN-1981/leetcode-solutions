403e7ea83a566c6e99f0b210eedd3f73ffb7fe9b
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-07-10 09:36:57 +0530

    Add squares-of-a-sorted-array [Array] [Easy] (Java)

---
+++ Array/Easy/squares-of-a-sorted-array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        
+        for(int i=0; i<nums.length; i++){
+            int n = (int) Math.pow(nums[i] , 2);
+            nums[i] = n;
+        }
+        Arrays.sort(nums);
+
+        return nums;
+    }
+}
