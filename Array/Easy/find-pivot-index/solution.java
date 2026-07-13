e4a1c3ad61dca045dac1b175835ee0dd5d260a42
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-07-13 13:17:59 +0530

    Add find-pivot-index [Array] [Easy] (Java)

---
+++ Array/Easy/find-pivot-index/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int pivotIndex(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int left = leftSum(arr, i - 1);
+            int right = rightSum(arr, i + 1, n);
+
+            if (left == right) {
+                return i;
+            } 
+        }
+        return -1;
+    }
+
+    public static int leftSum(int arr[], int index) {
+        int sum = 0;
+
+        if (index >= 0) {
+            sum = arr[index] + leftSum(arr, index - 1);
+        }
+        return sum;
+    }
+
+    public static int rightSum(int arr[], int index, int n) {
+        int sum = 0;
+
+        if (index <= n - 1) {
+            sum = arr[index] + rightSum(arr, index + 1, n);
+        }
+        return sum;
+    }
+
+}
