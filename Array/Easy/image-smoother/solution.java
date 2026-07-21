a2aab8db717fc3b3af75ae73f3be07fdc7e36b1a
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-07-21 22:13:50 +0530

    Add image-smoother [Array] [Easy] (Java)

---
+++ Array/Easy/image-smoother/solution.java
@@ -0,0 +1,109 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+
+        int rows = img.length;
+        int cols = img[0].length;
+
+        int smoothed[][] = new int[rows][cols];
+
+        if (rows == 1 && cols == 1) {
+            smoothed[0][0] = img[0][0];
+        }
+
+        else if (rows == 1) {
+
+            for (int col = 0; col < cols; col++) {
+
+                if (col == 0) {
+                    smoothed[0][col] = (img[0][col] + img[0][col + 1]) / 2;
+                } else if (col == cols - 1) {
+                    smoothed[0][col] = (img[0][col] + img[0][col - 1]) / 2;
+                } else {
+                    smoothed[0][col] = (img[0][col - 1] + img[0][col] + img[0][col + 1]) / 3;
+                }
+            }
+        }
+
+        else if (cols == 1) {
+
+            for (int row = 0; row < rows; row++) {
+
+                if (row == 0) {
+                    smoothed[row][0] = (img[row][0] + img[row + 1][0]) / 2;
+                } else if (row == rows - 1) {
+                    smoothed[row][0] = (img[row][0] + img[row - 1][0]) / 2;
+                } else {
+                    smoothed[row][0] = (img[row - 1][0] + img[row][0] + img[row + 1][0]) / 3;
+                }
+            }
+        } else {
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+
+                    if (row == 0 && col == 0) {
+                        int sum = img[row][col] + img[row][col + 1]
+                                + img[row + 1][col] + img[row + 1][col + 1];
+                        smoothed[row][col] = sum / 4;
+                    }
+
+                    else if (row == 0 && col == cols - 1) {
+                        int sum = img[row][col] + img[row][col - 1]
+                                + img[row + 1][col] + img[row + 1][col - 1];
+                        smoothed[row][col] = sum / 4;
+                    }
+
+                    else if (row == rows - 1 && col == 0) {
+                        int sum = img[row][col] + img[row - 1][col]
+                                + img[row][col + 1] + img[row - 1][col + 1];
+                        smoothed[row][col] = sum / 4;
+                    }
+
+                    else if (row == rows - 1 && col == cols - 1) {
+                        int sum = img[row][col] + img[row - 1][col]
+                                + img[row][col - 1] + img[row - 1][col - 1];
+                        smoothed[row][col] = sum / 4;
+                    }
+
+                    else if (row == 0) {
+                        int sum = img[row][col - 1] + img[row][col] + img[row][col + 1]
+                                + img[row + 1][col - 1] + img[row + 1][col]
+                                + img[row + 1][col + 1];
+                        smoothed[row][col] = sum / 6;
+                    }
+
+                    else if (row == rows - 1) {
+                        int sum = img[row][col - 1] + img[row][col] + img[row][col + 1]
+                                + img[row - 1][col - 1] + img[row - 1][col]
+                                + img[row - 1][col + 1];
+                        smoothed[row][col] = sum / 6;
+                    }
+
+                    else if (col == 0) {
+                        int sum = img[row - 1][col] + img[row][col] + img[row + 1][col]
+                                + img[row - 1][col + 1] + img[row][col + 1]
+                                + img[row + 1][col + 1];
+                        smoothed[row][col] = sum / 6;
+                    }
+
+                    else if (col == cols - 1) {
+                        int sum = img[row - 1][col] + img[row][col] + img[row + 1][col]
+                                + img[row - 1][col - 1] + img[row][col - 1]
+                                + img[row + 1][col - 1];
+                        smoothed[row][col] = sum / 6;
+                    }
+
+                    else {
+                        int sum = img[row - 1][col - 1] + img[row - 1][col]
+                                + img[row - 1][col + 1] + img[row][col - 1]
+                                + img[row][col] + img[row][col + 1]
+                                + img[row + 1][col - 1] + img[row + 1][col]
+                                + img[row + 1][col + 1];
+
+                        smoothed[row][col] = sum / 9;
+                    }
+                }
+            }
+        }
+        return smoothed;
+    }
+}
