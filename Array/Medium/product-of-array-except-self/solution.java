56224441e1526f2db6ce6ac62bbb86d33d9eecf2
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-09 14:15:40 +0530

    Add product-of-array-except-self [Array] [Medium] (Java)

---
+++ Array/Medium/product-of-array-except-self/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+
+        int preffix[] = new int[n];
+        int suffix[] = new int[n];
+
+        preffix[0] = 1;
+        suffix[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            preffix[i] = preffix[i - 1] * nums[i - 1];
+
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = preffix[i] * suffix[i];
+        }
+
+        return nums;
+    }
+}
