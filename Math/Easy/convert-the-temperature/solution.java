66910e04e2888a8722afda1230fafe9396fa9c07
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-15 21:07:36 +0530

    Add convert-the-temperature [Math] [Easy] (Java)

---
+++ Math/Easy/convert-the-temperature/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        
+        double[] sol = new double[2];
+
+        sol[0] = celsius + 273.15;
+        sol[1] = celsius * 1.80 + 32.00;
+
+        return sol;
+
+    }
+}
