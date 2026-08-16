2471a7ab0456db7b46b9d37a1a6c3ea5b43061e4
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-16 22:13:02 +0530

    Add number-of-good-pairs [Array] [Easy] (Java)

---
+++ Array/Easy/number-of-good-pairs/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+
+        int count = 0;
+        int arr[] = new int[101];
+
+        for(int i=0; i<nums.length; i++){
+            count += arr[nums[i]];
+            arr[nums[i]]++;
+        }
+        return count;
+    }
+}
