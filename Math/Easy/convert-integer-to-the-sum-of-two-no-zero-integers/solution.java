ce066d4b72e8aed218bda9856cdc22b2228ccd74
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-28 11:02:42 +0530

    Add convert-integer-to-the-sum-of-two-no-zero-integers [Math] [Easy] (Java)

--- Math/Easy/convert-integer-to-the-sum-of-two-no-zero-integers/solution.java
+++ Math/Easy/convert-integer-to-the-sum-of-two-no-zero-integers/solution.java
@@ -6,20 +6,20 @@ class Solution {
         
         for(int i=0; i<arr.length; i++){
             if(String.valueOf(arr[i]).contains("0") == false){
-            for(int j=0; j<arr.length; j++){
-                if(String.valueOf(arr[j]).contains("0") == false){
-            if(arr[i] + arr[j] == n){
-                return new int[] {arr[i] , arr[j]};
-            }
+                for(int j=0; j<arr.length; j++){
+                    if(String.valueOf(arr[j]).contains("0") == false){
+                        if(arr[i] + arr[j] == n){
+                            return new int[] {arr[i] , arr[j]};
+                        }
+                    }
                 }
             }
-            }
         }
-        
+        System.gc();
         return new int[0];
     }
 
-    public static void enter(int n, int[] arr) {
+public static void enter(int n, int[] arr) {
         if (n == 0) {
             return;
         }
@@ -28,4 +28,4 @@ class Solution {
 
         enter(n - 1, arr);
     }
-    }
+}
